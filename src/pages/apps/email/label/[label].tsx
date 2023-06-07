ee6d6fef46de5fddb7cc3cbccde27d2894e85170
EmiCon97 <derleuchtturm97@gmail.com>
2023-06-06 21:40:36 -0300

    no-url

--- src/pages/apps/email/label/[label].tsx
+++ src/pages/apps/email/label/[label].tsx
@@ -1,41 +1,41 @@
 // ** Next Import
-import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next/types'
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next/types';
 
 // ** Third Party Imports
-import axios from 'axios'
+import axios from 'axios';
 
 // ** Types
-import { MailLayoutType, MailType } from 'src/types/apps/emailTypes'
+import { MailLayoutType, MailType } from 'src/types/apps/emailTypes';
 
 // ** Demo Components Imports
-import Email from 'src/views/apps/email/Email'
+import Email from 'src/views/apps/email/Email';
 
 const EmailApp = ({ label }: MailLayoutType) => {
-  return <Email label={label} />
-}
+  return <Email label={label} />;
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const res = await axios.get('http://localhost:3000/apps/email/allEmails')
-  const data: MailType[] = await res.data.emails
+  const res = await axios.get('/apps/email/allEmails');
+  const data: MailType[] = await res.data.emails;
 
   const paths = data.map((mail: MailType) => ({
     params: { label: mail.labels[0] }
-  }))
+  }));
 
   return {
     paths,
     fallback: false
-  }
-}
+  };
+};
 
 export const getStaticProps: GetStaticProps = ({ params }: GetStaticPropsContext) => {
   return {
     props: {
       ...(params && params.label ? { label: params.label } : {})
     }
-  }
-}
+  };
+};
 
-EmailApp.contentHeightFixed = true
+EmailApp.contentHeightFixed = true;
 
-export default EmailApp
+export default EmailApp;
