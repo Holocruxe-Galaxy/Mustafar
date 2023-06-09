2102c7e3d324185b432d449ae780d0e672d2ebec
solanamr <romerosolana@gmail.com>
2023-06-09 15:05:09 -0300

    merging

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,14 +1,19 @@
 import Model from '../../@core/components/holocruxe-model/index'
-
+import { ReactNode } from 'react'
+import BlankLayout from 'src/@core/layouts/BlankLayout'
 
 
 
 const Test = () => {
   return (
-    <div>
+    <>
       <Model/>
-    </div>
+    </>
   );
 }
+
+Test.getLayout = (page: ReactNode) => <BlankLayout>{page}</BlankLayout>
+
+Test.guestGuard = true
 
 export default Test;
