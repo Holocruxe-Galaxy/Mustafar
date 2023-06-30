927f570f9bb81d8e2a9648008d2c545f42683b0c
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2023-06-30 19:37:07 -0300

    auth/localhost

--- src/@core/components/Auth0/Provider.tsx
+++ src/@core/components/Auth0/Provider.tsx
@@ -7,7 +7,7 @@ const ProviderAuth0 = ({ children }: { children: React.ReactNode }) => {
       domain='dev-7vk8vxpbjwabxxmu.us.auth0.com'
       clientId='viIoEU0gLFUQn9NGOOYjO0USFvheZxIM'
       authorizationParams={{
-        redirect_uri: 'https://alex-auth.d19n3mgta93hy5.amplifyapp.com'
+        redirect_uri: 'https://alex-auth.d19n3mgta93hy5.amplifyapp.com/login'
       }}
     >
       {children}
