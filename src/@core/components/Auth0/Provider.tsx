967f612c855a164b42c0c864a78e56a59d7514b1
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2023-06-30 17:47:36 -0300

    arreglando auth0 redirect

--- src/@core/components/Auth0/Provider.tsx
+++ src/@core/components/Auth0/Provider.tsx
@@ -1,14 +1,12 @@
 'use client'
-
 import { Auth0Provider } from '@auth0/auth0-react'
-
 const ProviderAuth0 = ({ children }: { children: React.ReactNode }) => {
   return (
     <Auth0Provider
       domain='dev-7vk8vxpbjwabxxmu.us.auth0.com'
       clientId='viIoEU0gLFUQn9NGOOYjO0USFvheZxIM'
       authorizationParams={{
-        redirect_uri: 'http://localhost:3000/login'
+        redirect_uri: 'https://alex-auth.d19n3mgta93hy5.amplifyapp.com/login'
       }}
     >
       {children}
