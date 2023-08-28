57f3867c0bf972e76d51edbd3f40f40cf64a1323
EmiCon97 <derleuchtturm97@gmail.com>
2023-08-28 18:13:45 -0300

    fix: return in on submit function deleted

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -496,8 +496,6 @@ const Register = () => {
   const onSubmit = async (data: any) => {
     const manager = stepManager(activeStep, data, caract);
 
-    return;
-
     try {
       const token = localStorage.getItem('AuthorizationToken');
 
