ba0bda726363089c0675247845610fd4cbe1b2d6
Ailín <ailin.tobares@gmail.com>
2023-09-11 19:00:23 -0300

    style box-shadow in cardbuttons

--- src/views/components/horizontalBar/CardButtons.tsx
+++ src/views/components/horizontalBar/CardButtons.tsx
@@ -40,7 +40,7 @@ const CardButtons = (props: CardButtonsProps) => {
       position: 'relative',
       backgroundColor: 'background.paper',
       borderRadius: '10px',
-      boxShadow: skin === 'bordered' ? 0 : 6,
+      boxShadow: /* skin === 'bordered' ? 0 : 6 */ '4px 4px 4px 0px rgba(255, 255, 255, 0.50)',
       ...(skin === 'bordered' && { border: `1px solid ${theme.palette.divider}` })
     }}
     >
