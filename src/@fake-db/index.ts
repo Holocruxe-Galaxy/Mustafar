28c9999991f60e8dd405fca855533fedf07091f7
EmiCon97 <derleuchtturm97@gmail.com>
2023-09-20 17:43:22 -0300

    refactor: removed unused imports and user profile component. building

---
+++ src/@fake-db/index.ts
@@ -0,0 +1,10 @@
+import mock from './mock'
+
+import './auth/jwt'
+import './email/email'
+import './edit/edit'
+import './add/add'
+import './user-list/user-list'
+import './pages/profile'
+
+mock.onAny().passThrough()
