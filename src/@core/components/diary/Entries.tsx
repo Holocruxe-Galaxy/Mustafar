6cef028d53c224292068602e7a4b3344b2642766
EmiCon97 <derleuchtturm97@gmail.com>
2023-09-29 18:19:29 -0300

    fix: not showing new photo after update

--- src/@core/components/diary/Entries.tsx
+++ src/@core/components/diary/Entries.tsx
@@ -218,12 +218,12 @@ const Entries = ({ id, props }: any) => {
 
     if (data.emoji === undefined || data.emoji === props.emoji) delete data.emoji
 
-    if (!data.photos.length && !props.photos.length && data.photos === data.photos) delete data.photos
+    if ((!data.photos.length && !props.photos.length) || data.photos.length === data.photos.length) delete data.photos
 
     if (data.favorite === props.favorite) delete data.favorite
 
     if(file) {
-      dispatch(editEntrieWithFile({ ...data, _id: id, file }))
+      dispatch(editEntrieWithFile({ ...data, _id: id, photos: [], file }))
       setFile(undefined)
     }
     if (!file) dispatch(editEntrie({ ...data, _id: id }))
@@ -362,21 +362,7 @@ const Entries = ({ id, props }: any) => {
 
             <Modal open={openEdit} onClose={handleCloseEdit} sx={styleModal}>
               <Box sx={styleEdit} component='div'>
-                <Box
-                  component='div'
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                    mb: 5
-                  }}
-                >
-                  <Typography sx={{ color: '#F836F4' }}>EDITAR</Typography>
-                  <IconButton onClick={handleCloseEdit} sx={{ color: '#00FFED' }}>
-                    <ClearIcon />
-                  </IconButton>
-                </Box>
+                <Typography sx={{ textAlign: 'left', mb: 5, color: '#F836F4' }}>EDITAR</Typography>
                 <form onSubmit={handleSubmit(onSubmit)}>
                   <FormControl>
                     <Controller
@@ -527,10 +513,7 @@ const Entries = ({ id, props }: any) => {
                       >
                         {fields.map((field, index) => (
                           <>
-                            <IconButton
-                              onClick={() => remove(index)}
-                              sx={{ float: 'right', mr: 2, mb: 2, color: '#00FFED' }}
-                            >
+                            <IconButton onClick={() => remove(index)} sx={{ float: 'right', mr: 2, mb: 2 }}>
                               <ClearIcon />
                             </IconButton>
                             <CardContent key={field.id}>
