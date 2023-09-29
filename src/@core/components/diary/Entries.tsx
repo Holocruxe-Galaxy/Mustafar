d372b3e3c0c53adfde5c3552f5867ae29508f72f
EmiCon97 <derleuchtturm97@gmail.com>
2023-09-29 19:30:59 -0300

    fix: edit breaking img

--- src/@core/components/diary/Entries.tsx
+++ src/@core/components/diary/Entries.tsx
@@ -217,8 +217,8 @@ const Entries = ({ id, props }: any) => {
     if (data.content === props.content) delete data.content
 
     if (data.emoji === undefined || data.emoji === props.emoji) delete data.emoji
-
-    if ((!data.photos.length && !props.photos.length) || data.photos.length === data.photos.length) delete data.photos
+    
+    if(data.photos.length) delete data.photos
 
     if (data.favorite === props.favorite) delete data.favorite
 
