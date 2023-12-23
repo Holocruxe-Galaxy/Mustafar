68b1cb23d87532b6e5ba83cf3be7fe94a9afe624
MatiCh01 <exechoco17@gmail.com>
2023-12-22 21:03:22 -0300

    style:Button color change

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1382,7 +1382,17 @@ const Register = () => {
           <Typography>Se completaron los pasos con éxito!</Typography>
           <Box component='div' sx={{ mt: 4, display: 'flex', justifyContent: 'flex-end' }}>
             <Link href='/home'>
-              <Button size='large' variant='contained'>
+              <Button
+                size='large'
+                variant='contained'
+                sx={{
+                  '&:hover': {
+                    color: '#00FFED',
+                    background: 'linear-gradient(180deg, #00FFED 0%, rgba(248, 54, 244, 0.20) 100%)',
+                    boxShadow: '4px 4px 25px 0px rgba(0, 0, 0, 0.70), 4px 4px 4px 0px rgba(255, 255, 255, 0.25) inset'
+                  }
+                }}
+              >
                 Dashboard
               </Button>
             </Link>
